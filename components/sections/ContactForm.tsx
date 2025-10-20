a4aa9b5a130976db44d044cdf151b1c4dbea957c
Chinemerem Ugbaja <marycynthiaconfirm@gmail.com>
2025-10-20 12:57:13 +0000

    Add disabled to the form button when it is submitting to avoid duplicate submission

--- components/sections/ContactForm.tsx
+++ components/sections/ContactForm.tsx
@@ -1,9 +1,9 @@
 "use client";
 import { useState } from "react";
 import emailjs from "@emailjs/browser";
-import { error } from "console";
 
 const ContactForm = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false)
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -20,7 +20,7 @@ const ContactForm = () => {
 
   const handleForm = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
+    setIsSubmitting(true)
     try {
       await emailjs.send(
         "service_bu69xum",
@@ -36,6 +36,7 @@ const ContactForm = () => {
         subject: "",
         message: "",
       });
+      setIsSubmitting(false)
     } catch (error) {
       alert('Message failed to send')
     }
@@ -93,7 +94,9 @@ const ContactForm = () => {
         className="rounded-md min-h-[150px] resize-none  w-full p-4  border-[#566265] border focus:outline-none focus:border-[#FEB273] mb-8"
       ></textarea>
 
-      <button className=" w-fit font-medium capitalize flex gap-3 pr-3 items-center border border-[#FEB273] rounded-[100px]">
+      <button className= {`w-fit font-medium capitalize flex gap-3 pr-3 items-center border border-[#FEB273] rounded-[100px] ${isSubmitting? "opacity-40": ""} `}
+      disabled = {isSubmitting}
+      >
         <span
           className={`text-black bg-[#FEB273] grid place-content-center py-5 rounded-tr-[150px] rounded-tl-[100px] rounded-bl-[100px] px-12`}
         >
